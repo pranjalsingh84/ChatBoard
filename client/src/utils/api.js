8cf4ad965a7a8b17c9ccdc91b3dcc7651e378437
pranjalsingh84 <topranjal.singh@gmail.com>
2026-01-21 12:54:25 +0530

    fixed api base url for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,30 +1,46 @@
 import axios from "axios";
 
-// Render backend URL
+// 🔥 LIVE BACKEND URL
 const apiBaseURL = "https://chatboard-zewg.onrender.com";
 
-export const GET = (url, header, callback, errorcallback) => {
+export const GET = (url, header = {}, callback, errorcallback) => {
   return axios
-    .get(`${apiBaseURL}/${url}`, {
+    .get(`${apiBaseURL}${url}`, {
       headers: header,
     })
     .then((response) => {
       if (callback) callback(response);
     })
     .catch((err) => {
+      console.error("GET Error:", err.response?.data || err.message);
       if (errorcallback) errorcallback(err);
     });
 };
 
-export const POST = (url, data, header, callback, errorcallback) => {
+export const POST = (url, data, header = {}, callback, errorcallback) => {
   return axios
-    .post(`${apiBaseURL}/${url}`, data, {
+    .post(`${apiBaseURL}${url}`, data, {
       headers: header,
     })
     .then((response) => {
       if (callback) callback(response);
     })
     .catch((err) => {
+      console.error("POST Error:", err.response?.data || err.message);
+      if (errorcallback) errorcallback(err);
+    });
+};
+
+export const DELETE = (url, header = {}, callback, errorcallback) => {
+  return axios
+    .delete(`${apiBaseURL}${url}`, {
+      headers: header,
+    })
+    .then((response) => {
+      if (callback) callback(response);
+    })
+    .catch((err) => {
+      console.error("DELETE Error:", err.response?.data || err.message);
       if (errorcallback) errorcallback(err);
     });
 };
