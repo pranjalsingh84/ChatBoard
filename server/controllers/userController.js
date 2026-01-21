ee9e7a56f1aac35739f83e4aeb977db2f73960c2
pranjalsingh84 <topranjal.singh@gmail.com>
2026-01-21 15:14:39 +0530

    fix auth: use bcrypt and jsonwebtoken

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("User");   // ✅ VERY IMPORTANT LINE
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // ================= LOGIN =================
